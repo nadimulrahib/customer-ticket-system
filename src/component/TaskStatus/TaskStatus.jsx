795444894d227e8f25b2227d62d4672db04d0e95
Nadimul Rahib <nadimulrahib38@gmail.com>
2026-06-29 22:15:01 +0600

    added

--- src/component/TaskStatus/TaskStatus.jsx
+++ src/component/TaskStatus/TaskStatus.jsx
@@ -1,22 +1,22 @@
 import TaskCards from "../TaskCards/TaskCards";
 
-const TaskStatus = ({ cardClick,handleCompleted }) => {
+const TaskStatus = ({ cardClick, handleCompleted }) => {
 
   return (
     <div>
       <div className="title flex flex-col gap-3.5">
         <p className="task text-2xl">Task Status</p>
 
-    {
-      cardClick.length===0?<p>Select a ticket to add to Task Status</p>     
-     : <div className="customer flex flex-col gap-5">
-        {cardClick.map((customer) => (
-          <TaskCards customer={customer} handleCompleted={handleCompleted}></TaskCards>
-        ))}
-      </div>
-    }
+        {
+          cardClick.length === 0 ? <p>Select a ticket to add to Task Status</p>
+            : <div className="customer flex flex-col gap-5">
+              {cardClick.map((customer) => (
+                <TaskCards customer={customer} handleCompleted={handleCompleted}></TaskCards>
+              ))}
+            </div>
+        }
 
-   
+
       </div>
 
     </div>
